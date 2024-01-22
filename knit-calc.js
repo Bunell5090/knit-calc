66aff69d76ed53164d09b14f344b6814cd15091e
Ben Unell <97351508+Bunell5090@users.noreply.github.com>
2024-01-21 20:11:33 -0600

    Add if else logic to js file

--- knit-calc.js
+++ knit-calc.js
@@ -20,6 +20,37 @@ function knit() {
   var substitutedWeightNumber = parseFloat(substitutedWeight);
   var substitutedLength = document.getElementById("substitutedLength").value;
   var substitutedLengthNumber = parseFloat(substitutedLength);
+  var substitutedLengthAnswer = suggestedLengthNumber;
+
+  console.log("Suggested Weight Type: " + suggestedWeightType);
+  console.log("Suggested Weight Number: " + suggestedWeightNumber);
+  console.log("Suggested Length Type: " + suggestedLengthType);
+  console.log("Suggested Length Number: " + suggestedLengthNumber);
+  console.log("Substituted Weight Type: " + substitutedWeightType);
+  console.log("Substituted Weight Number: " + substitutedWeightNumber);
+  console.log("Substituted Length Type: " + substitutedLengthType);
+  console.log("Substituted Length Number: " + substitutedLengthNumber);
+  if (
+    suggestedWeightType === substitutedWeightType &&
+    suggestedLengthType === substitutedLengthType
+  ) {
+    var substitutedWeightAnswer =
+      (substitutedLengthNumber / substitutedWeightNumber) *
+      suggestedLengthNumber;
+    console.log("Answer:" + substitutedWeightAnswer);
+  } else if (
+    suggestedWeightType === substitutedWeightType &&
+    suggestedLengthType != substitutedLengthType
+  ) {
+  } else if (
+    suggestedWeightType != substitutedWeightType &&
+    suggestedLengthType === substitutedLengthType
+  ) {
+  } else if (
+    suggestedWeightType != substitutedWeightType &&
+    suggestedLengthType != substitutedLengthType
+  ) {
+  }
 
   console.log("Suggested Weight Type: " + suggestedWeightType);
   console.log("Suggested Weight Number: " + suggestedWeightNumber);
